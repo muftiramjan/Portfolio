6af823d530908e47f2dc91d6ea67dbced2c468f7
mufti ramjan <alibinalaluddin@gmail.com>
2024-10-13 22:07:00 +0600

    contact form added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,10 @@
-// import ContactForm from '@/components/ContactForm';
+import ContactForm from '@/components/ContactForm';
 import React from 'react';
 
 const Contact= () => {
     return (
         <div>
-           {/* <ContactForm></ContactForm> */}
-           Hello
+           <ContactForm></ContactForm>
         </div>
     );
 };
